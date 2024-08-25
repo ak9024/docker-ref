6191e017be2face367b509ca7ac915547883165b
Adiatma Kamarudin <adiatma.mail@gmail.com>
2024-08-25 12:35:08 +0800

    add Dockerfile.optimize in rust apps to reduce sizes

--- apps/rust/src/main.rs
+++ apps/rust/src/main.rs
@@ -1,28 +1,44 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    body::BoxBody, get, http::header::ContentType, middleware::Logger, App, HttpResponse,
+    HttpServer, Responder,
+};
+use env_logger::Env;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Data {
+    name: &'static str,
+}
+
+impl Responder for Data {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &actix_web::HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn index() -> impl Responder {
+    Data {
+        name: "Hello World",
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .service(index)
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("0.0.0.0", 8081))?
     .run()
     .await
 }
